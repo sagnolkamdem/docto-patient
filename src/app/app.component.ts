0feb7136a706ef032fd73f5f852ac8e726f071c4
Yannick NKENDEM <lovenkendemyannick@gmail.com>
2023-02-22 06:40:01 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'code.tabiblib.frontend.web.pro.v1.angular';
+}
