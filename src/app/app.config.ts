18e43a28a4157d86baa83ab74375f97f56b4e849
chifoidriss <chifoidriss@gmail.com>
2023-03-08 17:49:19 +0100

    agenda component design

---
+++ src/app/app.config.ts
@@ -0,0 +1,9 @@
+import { CalendarConfig, ProjectModelConfig } from '@bryntum/calendar';
+
+export const projectConfig: Partial<ProjectModelConfig> = {
+    // Empty project config
+};
+
+export const calendarConfig: Partial<CalendarConfig> = {
+    date: new Date(2022, 0, 1)
+};
