c625b8c74116e7f6d135c2c54749cac47ac0eda8
chifoidriss <chifoidriss@gmail.com>
2023-03-10 17:17:51 +0100

    remove package @bryntum/calendar-angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { MaterialModule } from './modules/material/material.module';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BryntumCalendarModule } from '@bryntum/calendar-angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -12,8 +11,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserAnimationsModule,
     MaterialModule,
-    AppRoutingModule,
-    BryntumCalendarModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
