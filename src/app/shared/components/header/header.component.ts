c45c24b2868c34c03d1bd460a3322724a676f946
chifoidriss <chifoidriss@gmail.com>
2023-03-15 12:13:29 +0100

    Add responive on menu and sidebar

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    $('.menu-burger').on('click', (e) => {
+      $('.page-content .sidebar').toggleClass('open');
+      $('.overlay').toggleClass('visible');
+    });
   }
 
 }
